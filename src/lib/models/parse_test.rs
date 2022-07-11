469a28c46ac599a4a1377db6f46aa4d21d8b2f1f
durid-ah <ahmad.durid96@gmail.com>
2022-07-10 23:17:48 -0600

    comment out map_ok in parser struct

--- src/lib/models/parse_test.rs
+++ src/lib/models/parse_test.rs
@@ -9,20 +9,20 @@ impl<'a, R1, R2, T, E1, E2> Parser<'a, R1, R2, T, E1, E2> {
       Self { transformer_fn }
    }
 
-   pub fn map_ok<S: 'a>(&'a mut self, mut map_fn: Box<dyn FnMut(Result<Cardinality<R2>, E2>) -> Result<Cardinality<S>, E2> + 'a>) -> Parser<'a,R1, S, T, E1, E2> {
+   // pub fn map_ok<S: 'a>(&'a mut self, mut map_fn: Box<dyn FnMut(Result<Cardinality<R2>, E2>) -> Result<Cardinality<S>, E2> + 'a>) -> Parser<'a,R1, S, T, E1, E2> {
       
-      let transformer = move |state: State<R1,T, E1>| {
-         let next = (self.transformer_fn)(state);
+   //    let transformer = move |state: State<R1,T, E1>| {
+   //       let next = (self.transformer_fn)(state);
 
-         let result = next.result
-            .and_then(|r| Some(map_fn(r)));
-         State{ index: next.index, target: next.target, result }
-      };
+   //       let result = next.result
+   //          .and_then(|r| Some(map_fn(r)));
+   //       State{ index: next.index, target: next.target, result }
+   //    };
 
-      let transformer = Box::new(transformer);
+   //    let transformer = Box::new(transformer);
 
-      return Parser::new(transformer);
-   }
+   //    return Parser::new(transformer);
+   // }
 }
 
 impl<'a, R1, R2, T, E1, E2> Transform<R1, R2, T, E1, E2> for Parser<'a, R1, R2, T, E1, E2> {
