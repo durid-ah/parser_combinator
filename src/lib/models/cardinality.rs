277b2aa6b0a511ccaaba0f69023f344c92f6c41b
durid-ah <ahmad.durid96@gmail.com>
2022-07-15 06:57:26 -0600

    added methods to the cardinality enum that unwrap its internal value

--- src/lib/models/cardinality.rs
+++ src/lib/models/cardinality.rs
@@ -3,3 +3,19 @@ pub enum Cardinality<R> {
    One(R),
    Many(Vec<R>)
 }
+
+impl<R> Cardinality<R> {
+   pub fn unwrap_one(self) -> R {
+      match self {
+         Cardinality::One(item) => item,
+         Cardinality::Many(_) => panic!("Cardinality::Attempted to unwrap one instead got many")
+      }
+   }
+
+   pub fn unwrap_many(self) -> Vec<R> {
+      match self {
+         Cardinality::One(_) => panic!("Cardinality::Attempted to unwrap many instead got one"),
+         Cardinality::Many(items) => items
+      }
+   }
+}
