f00666c5df195e77458d87b22cbfb9e3a8c9e30d
durid-ah <ahmad.durid96@gmail.com>
2022-07-15 06:56:49 -0600

    change map and chain helper closures to return parser trait

--- src/lib/parser_helpers.rs
+++ src/lib/parser_helpers.rs
@@ -4,8 +4,8 @@ use crate::models::{state::{ParserResult, State}, parser::Parser, parser_traits:
 /// Maps the result of a parser that implements the [`Parse`] trait and creates a
 /// generic [`Parser`] that will run the previous parser and return a state with 
 /// the mapped result
-pub fn map_result<'a, R1, R2, T, E1, E2, S, F, I>(mut parser: I, mut map_fn: F) -> Box<dyn Parse< R1, S, T, E1, E2> + 'a>
-   where F: FnMut(ParserResult<R2, E2>) -> ParserResult<S,E2> + 'a,
+pub fn map_result<'a, R1, R2, T, E1, E2, S, F, I>(mut parser: I, map_fn: F) -> Box<dyn Parse< R1, S, T, E1, E2> + 'a>
+   where F: Fn(ParserResult<R2, E2>) -> ParserResult<S,E2> + 'a,
          I: Parse<R1,R2,T,E1,E2> + 'a,
          R1: 'a, R2: 'a, T: 'a, E1: 'a, E2: 'a, S: 'a {
 
@@ -24,7 +24,7 @@ pub fn map_result<'a, R1, R2, T, E1, E2, S, F, I>(mut parser: I, mut map_fn: F) 
 /// of the first parser and returns a struct that implements the [`Parse`] trait.
 /// Returns a new generic [`Parser`] that executes the parsers after each other 
 pub fn chain_parser<'a,I,F,R1,R2,T,E1,E2,S>(mut parser: I, mut chain_fn: F) -> Box<dyn Parse<R1, S, T, E1, E2> + 'a>
-   where F: FnMut(& ParserResult<R2, E2>) -> Box<dyn Parse<R2, S, T, E2, E2> + 'a> + 'a,
+   where F: Fn(& ParserResult<R2, E2>) -> Box<dyn Parse<R2, S, T, E2, E2> + 'a> + 'a,
          I: Parse<R1,R2,T,E1,E2> + 'a,
          R1: 'a, R2: 'a, T: 'a, E1: 'a, E2: 'a, S: 'a {
 
