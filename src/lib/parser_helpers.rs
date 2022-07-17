01c56ac9237a142b165302e3b568dd0f7501ea5b
durid-ah <ahmad.durid96@gmail.com>
2022-07-16 21:09:07 -0600

    cleanup

--- src/lib/parser_helpers.rs
+++ src/lib/parser_helpers.rs
@@ -23,7 +23,7 @@ pub fn map_result<'a, R1, R2, T, E1, E2, S, F, I>(mut parser: I, map_fn: F) -> B
 /// Chains to parsers to each other through a closure that takes in the result 
 /// of the first parser and returns a struct that implements the [`Parse`] trait.
 /// Returns a new generic [`Parser`] that executes the parsers after each other 
-pub fn chain_parser<'a,I,F,R1,R2,T,E1,E2,S>(mut parser: I, mut chain_fn: F) -> Box<dyn Parse<R1, S, T, E1, E2> + 'a>
+pub fn chain_parser<'a,I,F,R1,R2,T,E1,E2,S>(mut parser: I, chain_fn: F) -> Box<dyn Parse<R1, S, T, E1, E2> + 'a>
    where F: Fn(& ParserResult<R2, E2>) -> Box<dyn Parse<R2, S, T, E2, E2> + 'a> + 'a,
          I: Parse<R1,R2,T,E1,E2> + 'a,
          R1: 'a, R2: 'a, T: 'a, E1: 'a, E2: 'a, S: 'a {
