bfa4155cafaa020dd8988ba5d0dad1553ec2c155
durid-ah <ahmad.durid96@gmail.com>
2022-07-23 20:18:56 -0600

    parse trait: default run definition

--- src/lib/models/parser_traits.rs
+++ src/lib/models/parser_traits.rs
@@ -1,8 +1,13 @@
+use std::rc::Rc;
 use super::state::State;
+
 
 pub trait Parse<R1,R2,T,E1,E2> {
    fn transform(&mut self, state: State<R1, T, E1>) -> State<R2, T, E2>;
-   fn run(&mut self, target: T) -> State<R2, T, E2>;
+   fn run(&mut self, target: T) -> State<R2, T, E2> {
+      let initial_state = State{target: Rc::new(target), index: 0, result: None };
+      return self.transform(initial_state);
+   }
 }
 
 
@@ -17,8 +22,3 @@ pub trait Parse<R1,R2,T,E1,E2> {
 //    fn chain<S, F>(&self, chain_fn: F) -> Self::P
 //       where F: FnMut(ParserResult<Self::R2, Self::E2>) -> Box<dyn Parser<R1=Self::R2, R2=S, T=Self::T, E1 = Self::E1, E2 = Self::E2>>;
 // }
-
-
-#[cfg(test)]
-mod tests {
-}
