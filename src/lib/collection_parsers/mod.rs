3843351a12820ca0f3622f66046c956bb8840ec9
durid-ah <ahmad.durid96@gmail.com>
2022-08-11 20:43:46 -0600

    sep_by_one: connect to module, copy over sep_by and rename

--- src/lib/collection_parsers/mod.rs
+++ src/lib/collection_parsers/mod.rs
@@ -3,3 +3,4 @@ pub mod many_parser;
 pub mod many_one_parser;
 pub mod sequence_of_parser;
 pub mod sep_by_parser;
+pub mod sep_by_one_parser;
---
+++ src/lib/collection_parsers/sep_by_one_parser.rs
@@ -0,0 +1,111 @@
+use std::rc::Rc;
+
+use crate::models::cardinality::Cardinality::{One, Many};
+use crate::models::parser_traits::Parse;
+use crate::models::state::State;
+
+
+pub struct SepByOne<R1,R2,T>{
+   separator: Box<dyn Parse<R1, R2, T>>,
+   separated: Box<dyn Parse<R1,R2,T>>
+}
+
+impl<R1,R2,T> SepByOne<R1,R2,T> {
+   pub fn new(separator: Box<dyn Parse<R1, R2, T>>, separated: Box<dyn Parse<R1,R2,T>>) -> Self {
+      Self { separator, separated }
+   } 
+}
+
+impl<R1,R2,T> Parse<R1,R2,T> for SepByOne<R1,R2,T> {
+   fn transform(&mut self, state: State<R1, T>) -> State<R2, T> {
+      let contains_error = state.is_error();
+      if contains_error {
+         return State::from_err_state(state);
+      }
+
+      let target = Rc::clone(&state.target);
+      let mut results: Vec<R2> = Vec::new();
+      let mut final_state: State<R1, T> = State {
+         index: state.index,
+         target: Rc::clone(&state.target),
+         result: None
+      };
+
+      loop {
+         let thing_we_want_state = self.separated.transform(final_state);
+         final_state = State {
+            index: thing_we_want_state.index,
+            target: Rc::clone(&target),
+            result: None,
+         };
+
+         match thing_we_want_state.result.unwrap() {
+            Ok(One(res)) => results.push(res),
+            Ok(Many(mut res)) => results.append(&mut res),
+            Err(_) => {
+
+               break;
+            }
+         }
+
+         let separator_state = self.separator.transform(final_state);
+         final_state = State {
+            index: separator_state.index,
+            target: Rc::clone(&target),
+            result: None,
+         };
+
+         if separator_state.result.unwrap().is_err() {
+            break;
+         }
+      }
+
+      return State {
+         index: final_state.index,
+         target,
+         result: Some(Ok(Many(results))),
+     };
+   }
+}
+
+#[cfg(test)]
+mod tests {
+   use crate::parsers::str_parser::Str;
+   use super::*;
+
+   #[test]
+   fn success() {
+      let comma = Str::new(",".to_owned());
+      let test_string = Str::new("Test".to_owned());
+      let mut sep_parser = SepByOne::new(Box::new(comma), Box::new(test_string));
+      let result = sep_parser.run("Test,Test,Test");
+
+      assert!(result.result.is_some());
+      assert_eq!(result.result.unwrap().unwrap().unwrap_many().len(), 3);
+      assert_eq!(result.index, 14);
+   }
+
+   #[test]
+   fn ends_with_separator_success() {
+      let comma = Str::new(",".to_owned());
+      let test_string = Str::new("Test".to_owned());
+      let mut sep_parser = SepByOne::new(Box::new(comma), Box::new(test_string));
+      let result = sep_parser.run("Test,Test,");
+
+      assert!(result.result.is_some());
+      assert_eq!(result.result.unwrap().unwrap().unwrap_many().len(), 2);
+      assert_eq!(result.index, 10);
+   }
+
+   #[test]
+   fn empty_success() {
+      let comma = Str::new(",".to_owned());
+      let test_string = Str::new("Test".to_owned());
+      let mut sep_parser = SepByOne::new(Box::new(comma), Box::new(test_string));
+      let result = sep_parser.run("");
+
+      assert!(result.result.is_some());
+      assert_eq!(result.result.unwrap().unwrap().unwrap_many().len(), 0);
+      assert_eq!(result.index, 0);
+   }
+}
