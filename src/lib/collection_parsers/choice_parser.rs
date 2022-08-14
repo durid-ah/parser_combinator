004e1180eb1d9ef95524d5b0d1b2f90a1bc23111
durid-ah <ahmad.durid96@gmail.com>
2022-08-13 19:27:43 -0600

    choice stub

---
+++ src/lib/collection_parsers/choice_parser.rs
@@ -0,0 +1,26 @@
+use std::rc::Rc;
+
+use crate::models::{parser_traits::Parse, state::State};
+
+
+
+pub struct Choice<R1,R2,T> {
+   parsers: Vec<Box<dyn Parse<R1,R2,T>>>
+}
+
+impl<R1,R2,T> Choice<R1,R2,T> {
+   pub fn new(parsers: Vec<Box<dyn Parse<R1,R2,T>>>) -> Self {
+      Self { parsers }
+   } 
+}
+
+impl<R1,R2,T> Parse<R1,R2,T> for Choice<R1,R2,T> {
+   fn transform(&mut self, state: State<R1, T>) -> State<R2, T> {
+     State {
+         index: 0,
+         target: Rc::clone(&state.target),
+         result: None
+     }
+   }
+}
+
